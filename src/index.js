5b4c3967f685dd5c341c3e0064ad358280cf2a5d
Vince Falconi <vince.falconi@gmail.com>
2019-09-19 16:04:10 -0500

    add html output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const nunjucks = require('nunjucks');
 const beLoud = (process.argv.indexOf('loud') >= 0);
 const path = require('path');
 const contrast = require("color-contrast-calc").ContrastChecker;
@@ -40,6 +41,8 @@ Object.keys(palette).forEach(testColorName => {
 		comparisonResults = {
 			colorA: testName,
 			colorB: comparisonName,
+			hexA: testColor,
+			hexB: comparison,
 			contrastRatio: ratio,
 			AA: (ratio >= standards.AA ? 'pass' : 'fail'),
 			AAA: (ratio >= standards.AAA ? 'pass' : 'fail'),
@@ -52,5 +55,8 @@ Object.keys(palette).forEach(testColorName => {
 });
 
 csvWriter.writeRecords(results).then(() => console.log('Results exported to contrast-report.csv'));
+
+const html = nunjucks.render('src/report-template.html', { reports: results });
+fs.writeFileSync('report.html', html);
 
 if (beLoud) console.table(results)
